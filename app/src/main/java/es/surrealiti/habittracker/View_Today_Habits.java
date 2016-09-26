aec2e24c290c540dfa53b056714353d8603d930d
Justin Barclay <justincbarclay@gmail.com>
2016-09-26 12:36:42 -0600

    Refactor main view to allow it to recieve a habit from the make_habit view

--- app/src/main/java/es/surrealiti/habittracker/View_Today_Habits.java
+++ app/src/main/java/es/surrealiti/habittracker/View_Today_Habits.java
@@ -1,11 +1,13 @@
 package es.surrealiti.habittracker;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -56,6 +58,21 @@ public class View_Today_Habits extends AppCompatActivity {
 //        this.allHabits.add(new Habit("This is a new habit", new Date()));
 //        this.habitAdapter.notifyDataSetChanged();
         Intent intent = new Intent(this, MakeHabit.class);
-        startActivity(intent);
+        startActivityForResult(intent, 1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                Habit newHabit= (Habit) data.getParcelableExtra("newHabit");
+                Log.i("info", String.valueOf(newHabit));
+                allHabits.add(newHabit);
+                habitAdapter.notifyDataSetChanged();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+                throw new RuntimeException();
+            }
+        }
     }
 }
