b1ce39088dc0446346b3c8c20acb8233c8a41b08
Justin Barclay <justincbarclay@gmail.com>
2016-09-30 15:50:26 -0600

    WIP

--- app/src/main/java/es/surrealiti/habittracker/MakeHabit.java
+++ app/src/main/java/es/surrealiti/habittracker/MakeHabit.java
@@ -3,6 +3,8 @@ package es.surrealiti.habittracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -17,40 +19,70 @@ public class MakeHabit extends AppCompatActivity {
     private Button doneButton;
     private EditText name;
     private ToggleButton monday;
+    private ToggleButton tuesday;
+    private ToggleButton wednesday;
+    private ToggleButton thursday;
+    private ToggleButton friday;
+    private ToggleButton saturday;
+    private ToggleButton sunday;
+
+    Habit habit = new Habit("");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.make_habit);
         doneButton = (Button) findViewById(R.id.done);
         monday = (ToggleButton) findViewById(R.id.Monday);
+        tuesday = (ToggleButton) findViewById(R.id.Tuesday);
+        wednesday = (ToggleButton) findViewById(R.id.Wednesday);
+        thursday = (ToggleButton) findViewById(R.id.Thursday);
+        friday = (ToggleButton) findViewById(R.id.Friday);
+
         name = (EditText) findViewById(R.id.habitName);
 
-        monday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        monday.setOnCheckedChangeListener(trackDay);
+        tuesday.setOnCheckedChangeListener(trackDay);
+        wednesday.setOnCheckedChangeListener(trackDay);
+        thursday.setOnCheckedChangeListener(trackDay);
+        friday.setOnCheckedChangeListener(trackDay);
+
+
+        name.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                habit.setName(s.toString());
+            }
         });
-        doneButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 done();
 
             }
         });
-//        Intent intent = getIntent();
-//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-//        TextView textView = new TextView(this);
-//        textView.setTextSize(40);
-//        textView.setText(message);
-//
-//        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
-//        layout.addView(textView);
     }
 
+    private CompoundButton.OnCheckedChangeListener trackDay = new CompoundButton.OnCheckedChangeListener(){
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            habit.trackDay(buttonView.getText().toString(), isChecked);
+        }
+    };
+
     private void done() {
-        Habit newHabit = new Habit(name.getText().toString());
+        if (name.getText().toString() == ""){
+            // Eventually add a pop up prompting user for name
+        }
         Intent intent = new Intent();
-        intent.putExtra("newHabit", newHabit);
+        intent.putExtra("newHabit", habit);
         setResult(RESULT_OK, intent);
         finish();
     }
--- app/src/main/java/es/surrealiti/habittracker/View_Today_Habits.java
+++ app/src/main/java/es/surrealiti/habittracker/View_Today_Habits.java
@@ -3,14 +3,10 @@ package es.surrealiti.habittracker;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -32,27 +28,31 @@ public class View_Today_Habits extends AppCompatActivity {
         habitsContainer = (ListView) findViewById(R.id.list_habits);
         newHabit = (Button) findViewById(R.id.add_habit_button);
 
+        habitsContainer.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println(allHabits.get(position).toString());
+                editHabit(position);
+            }
+        });
         newHabit.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 setResult(RESULT_OK);
                 addNewHabit();
-
             }
         });
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
+
     protected void onStart() {
         super.onStart();
         this.habitAdapter = new ArrayAdapter<Habit>(this, R.layout.single_habit, allHabits);
         habitsContainer.setAdapter(habitAdapter);
+    }
+
+    public void editHabit(int index){
+        Intent intent = new Intent(this, EditHabit.class);
+        intent.putExtra("Habit", allHabits.get(index));
+        startActivityForResult(intent, 2);
     }
     public void addNewHabit() {
 //        this.allHabits.add(new Habit("This is a new habit", new Date()));
@@ -60,6 +60,7 @@ public class View_Today_Habits extends AppCompatActivity {
         Intent intent = new Intent(this, MakeHabit.class);
         startActivityForResult(intent, 1);
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1) {
@@ -73,6 +74,8 @@ public class View_Today_Habits extends AppCompatActivity {
                 //Write your code if there's no result
                 throw new RuntimeException();
             }
+        } else if(requestCode == 2){
+
         }
     }
 }
