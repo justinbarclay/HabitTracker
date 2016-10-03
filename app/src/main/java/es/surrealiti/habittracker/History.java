071a396f8dbb9c82264970aca8b270dd480d32e6
Justin Barclay <justincbarclay@gmail.com>
2016-10-02 23:24:30 -0600

    I really should not have had Android Studio add these files automatically

---
+++ app/src/main/java/es/surrealiti/habittracker/History.java
@@ -0,0 +1,12 @@
+package es.surrealiti.habittracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/es/surrealiti/habittracker/habitsController.java
@@ -0,0 +1,67 @@
+package es.surrealiti.habittracker;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+/**
+ * Created by Justin on 2016-10-02.
+ */
+
+public class habitsController {
+    private static final String FILENAME = "file.sav";
+    HabitList allHabits;
+
+    public habitsController(){}
+
+    private void loadFromFile() {
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+
+            // Code from http://stackoverflow.com/questions/12384064/gson-convert-from-json-to-a-typed-arraylistt
+            Type listType = new TypeToken<ArrayList<NormalTweet>>(){}.getType();
+
+            tweetList = gson.fromJson(in,listType);
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            tweetList = new ArrayList<Tweet>();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
+    }
+
+    private void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    0);
+
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+            Gson gson = new Gson();
+            gson.toJson(weeklyHabits, out);
+            out.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
+    }
+}
